4ca98a91748ec882650dc86c19fa95700a73d9e7
davidcyl <david@iceboundflame.com>
2012-05-01 16:30:25 -0700

    camera test

--- PC/InterestDetect/interest-detect.cpp
+++ PC/InterestDetect/interest-detect.cpp
@@ -3,17 +3,33 @@
 
 int main ( int argc, char **argv )
 {
-	cvNamedWindow( "My Window", 1 );
-	IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
-	CvFont font;
-	double hScale = 1.0;
-	double vScale = 1.0;
-	int lineWidth = 1;
-	cvInitFont( &font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC,
-			hScale, vScale, 0, lineWidth );
-	cvPutText( img, "Hello World!", cvPoint( 200, 400 ), &font,
-			cvScalar( 255, 255, 0 ) );
-	cvShowImage( "My Window", img );
-	cvWaitKey();
+	CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);
+	if (!capture) {
+		fprintf(stderr, "ERROR: capture is NULL \n");
+		getchar();
+		return -1;
+	}
+	// Create a window in which the captured images will be presented
+	cvNamedWindow("mywindow", CV_WINDOW_AUTOSIZE);
+	// Show the image captured from the camera in the window and repeat
+	while (1) {
+		// Get one frame
+		IplImage* frame = cvQueryFrame(capture);
+		if (!frame) {
+			fprintf(stderr, "ERROR: frame is null...\n");
+			getchar();
+			break;
+		}
+		cvFlip(frame, frame, 1);
+		cvShowImage("mywindow", frame);
+		// Do not release the frame!
+		//If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
+		//remove higher bits using AND operator
+		if ((cvWaitKey(10) & 255) == 27)
+			break;
+	}
+	// Release the capture device housekeeping
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("mywindow");
 	return 0;
 }
