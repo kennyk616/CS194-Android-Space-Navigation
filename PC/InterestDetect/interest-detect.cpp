65b54548e306dfcb3467db10a988aad440a8e785
Kenny Kao <kennyk616@gmail.com>
2012-05-01 16:51:22 -0700

    display mars_rocks image

--- PC/InterestDetect/interest-detect.cpp
+++ PC/InterestDetect/interest-detect.cpp
@@ -1,8 +1,52 @@
 #include <cv.h>
 #include <highgui.h>
+#include <string>
+
+using namespace std;
+
+void displayImage(const char* imageLocation)
+{
+  IplImage* img = 0;
+  int height,width,step,channels;
+  uchar *data;
+  int i,j,k;
+
+  // load an image
+  img=cvLoadImage(imageLocation, 1);
+  if(!img){
+    printf("Could not load image file: %s\n",imageLocation);
+    exit(0);
+  }
+
+  // get the image data
+  height    = img->height;
+  width     = img->width;
+  step      = img->widthStep;
+  channels  = img->nChannels;
+  data      = (uchar *)img->imageData;
+  printf("Processing a %dx%d image with %d channels\n",height,width,channels);
+
+  // create a window
+  cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
+  cvMoveWindow("mainWin", 100, 100);
+/*
+  // invert the image
+  for(i=0;i<height;i++) for(j=0;j<width;j++) for(k=0;k<channels;k++)
+    data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
+*/
+  // show the image
+  cvShowImage("mainWin", img );
+
+  // wait for a key
+  cvWaitKey(0);
+
+  // release the image
+  cvReleaseImage(&img );
+}
 
 int main ( int argc, char **argv )
 {
+	/*
 	cvNamedWindow( "My Window", 1 );
 	IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
 	CvFont font;
@@ -15,5 +59,8 @@ int main ( int argc, char **argv )
 			cvScalar( 255, 255, 0 ) );
 	cvShowImage( "My Window", img );
 	cvWaitKey();
+	*/
+	string img = "mars_rocks.jpg";
+	displayImage(img.c_str());
 	return 0;
 }
